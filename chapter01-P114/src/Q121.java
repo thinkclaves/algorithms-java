2a32309f544d16221b9d4986be5d6ffb8d569890
YapingYang <yaping.yang@tianheng-uestc.com>
2017-06-20 22:39:54 +0800

    update test

---
+++ chapter01-P114/src/Q121.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+/**
+ * @ClassName:     gcd
+ * @Description:   计算两个非负整数p和q的最大公约数
+ * 
+ * @author         thinkclaves
+ * @version        V1.0  
+ * @Date           2017/06/20 
+ * @reference      Algorithms Fouth Edition
+ */
+public class Q121 {
+	
+	public static void main(String[] args) {
+		try {
+			System.out.print("Please Input N:");
+			int n =System.in.read();
+			System.out.println("Input:" + n);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
